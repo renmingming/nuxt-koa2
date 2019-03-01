0819a16b4eb4147f07cb7c2ec1323061c106d8f3
renmingming <renmingming@iyich.com>
2019-03-01 19:55:08 +0800

    no message

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const Koa = require('koa')
+// const koaStatic = require('koa-static')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-generic-session'
+import Redis from 'koa-redis'
+import json from 'koa-json'
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
+// const history = require('connect-history-api-fallback');
+
+
+const app = new Koa()
+
+  // session有关
+  app.keys = ['mt', 'keyskeys']
+  app.proxy = true
+  app.use(session({
+    key: 'mt', //session前缀
+    prefix: 'mt:uid',
+    store: new Redis() // 借助redis存储
+  }))
+  app.use(bodyParser({
+    extendTypes: ['json', 'from', 'text']
+  }))
+  app.use(json())
+
+  // mongoose.connect(dbConfig.dbs, {
+  //   useNewUrlParser: true
+  // })
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(app.env === 'production')
+
+async function start() {
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
+
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+      .then(res => {
+        console.log('nuxt服务器已经启动')
+      })
+      .catch(err => {
+        // logger.warn(err)
+        console.log('nuxt服务器已停止')
+      })
+  } else {
+    await nuxt.ready()
+  }
+  app.use(users.routes()).use(users.allowedMethods())
+  app.use(ctx => {
+    ctx.status = 200
+    ctx.respond = false // Bypass Koa's built-in response handling
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx.req, ctx.res)
+  })
+ 
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+
+start()
